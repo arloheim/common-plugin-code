202edcd02bd0ddb398e24dd0cefb39446e32c750
Danae Dekker <hello@danaedekker.com>
2024-11-05 18:59:19 +0100

    Added default messages to loading messages from a configuration section

--- src/main/java/dev/danae/common/messages/ConfigurationMessageManager.java
+++ src/main/java/dev/danae/common/messages/ConfigurationMessageManager.java
@@ -1,7 +1,13 @@
 package dev.danae.common.messages;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 
 
 public interface ConfigurationMessageManager extends MessageManager
@@ -17,7 +23,7 @@ public interface ConfigurationMessageManager extends MessageManager
   }
 
   // Load the messages from the specified configuration section
-  public default void loadMessagesFromConfiguration(ConfigurationSection section)
+  public default void loadMessagesFromConfiguration(ConfigurationSection section, Map<String, String> defaultMessages)
   {
     this.getMessages().clear();
 
@@ -25,9 +31,23 @@ public interface ConfigurationMessageManager extends MessageManager
     {
       for (var name : section.getKeys(false))
       {
-        var message = section.getString(name);
+        var message = section.getString(name, defaultMessages.get(name));
         this.getMessages().put(name, message);
       }
     }
+  }
+
+  // Load the messages from the specified configuration file
+  public default void loadMessagesFromConfiguration(File file, Map<String, String> defaultMessages) throws FileNotFoundException, IOException, InvalidConfigurationException
+  {
+    var configuration = new YamlConfiguration();
+    configuration.load(file);
+    this.loadMessagesFromConfiguration(configuration, defaultMessages);
+  }
+  
+  // Load the messages from the specified configuration file in the plugin data folder
+  public default void loadMessagesFromConfiguration(Plugin plugin, String file, Map<String, String> defaultMessages) throws FileNotFoundException, IOException, InvalidConfigurationException
+  {
+    this.loadMessagesFromConfiguration(new File(plugin.getDataFolder(), file), defaultMessages);
   }
 }
